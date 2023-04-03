eab032011c875e97d698ab606f31bd8b52e68b2f
Juan Ignacio Tejada <jtejada@lubee.com.ar>
2023-04-03 19:10:10 -0300

    Se agregaron workers al archivo js, falta implementar base de datos, pensando en ver si se puede usar googlesheets para guardar informacion del form para agregar nuevos workers

--- src/data/projects.js
+++ src/data/projects.js
@@ -7,4 +7,44 @@ export const projectsData = [
     tag: "REFRIGERACION",
     number: "https://api.whatsapp.com/send?phone=542645801644",
   },
+  {
+    id: 2,
+    title: "Daniel",
+    category:
+      "Trabaja muy bien, es matriculado y cobra super barato para lo bien que trabaja. Se sabe todas las mañas y te va explicando todo lo que hace. Esto ocaciona que este con mucho trabajo todo el tiempo, por lo que es dificil engancharlo, pero si le hablas con tiempo te responde. ",
+    tag: "ELECTRICISTA",
+    number: "https://api.whatsapp.com/send?phone=542644463496",
+  },
+  {
+    id: 3,
+    title: "Emilio",
+    category:
+      "Hombre de total confianza, no te va a cagar. Soluciona los problemas relacionados a plomería. Me arregló una fuga que tenia en el bidet y luego lo llamamos para que haga unos arrelgos en la empresa, el vago cae con el hijo y un bolsito. Es puntual y cumple con las fechas",
+    tag: "PLOMERO",
+    number: "https://api.whatsapp.com/send?phone=542645173179",
+  },
+  {
+    id: 4,
+    title: "Jorge Albareti",
+    category:
+      "Este hombre se dedica a traer muebles platinum a San Juan. Estos muebles tienen una ventaja y es que ya vienen de forma estandar. Vos le hablas y te manda catálogo con precio. En el catálogo encontras: roperos, mesitas de luz, escritorios, etc.",
+    tag: "MUEBLES",
+    number: "https://api.whatsapp.com/send?phone=542646208623",
+  },
+  {
+    id: 5,
+    title: "Emiliano - Estilo rustico",
+    category:
+      "Se especializa en hacer muebles a medida, vos te imaginas algo o lo bucas por internet y el vago te manda presupuesto detallado y te lo hace. Yo me hice con el la barra, el escritorio y el recibidor de la entrada. Los precios mucho mas barato que comprar los mismos muebles en el easy por ejemplo, si decis que vas de mi parte te hace un descuento en la mano de obra .",
+    tag: "CARPINTERO",
+    number: "https://api.whatsapp.com/send?phone=542645292158",
+  },
+  {
+    id: 6,
+    title: "Luis",
+    category:
+      "Este hombre lidera una empresa familiar dedicada de toda la vida a hacer sillones. En mi caso personal necesitaba un sillón con medidas especificas para la pieza del tele. Trabajan impecable y te dan garantía de por vida. Podes elegir desde el tipo de tela y color, hasta el tipo de almohadon y su relleno. Un poco caro comparado con sillones que vienen estandar, pero son de mucho mejor calidad. Envios a domicilio sin cargo",
+    tag: "MUEBLES",
+    number: "https://api.whatsapp.com/send?phone=542644169375",
+  },
 ];
