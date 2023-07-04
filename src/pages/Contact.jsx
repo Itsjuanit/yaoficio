6038bc64ea2e926ffeb3875e50606fdb29ec1874
Juan Ignacio Tejada <jtejada@lubee.com.ar>
2023-07-04 16:38:23 -0300

    .

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,7 @@ const Contact = () => {
       transition={{
         ease: "easeInOut",
         duration: 0.5,
-        delay: 0.1,
+        delay: 0.11,
       }}
       className="container mx-auto flex flex-col-reverse lg:flex-row py-5 lg:py-10 lg:mt-10"
     ></motion.div>
