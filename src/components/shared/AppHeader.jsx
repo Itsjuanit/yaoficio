2ccc221b9c7d0ba4ac31428ddae09046c42452b7
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2023-08-03 22:47:06 -0300

    Cambios en varias partes del codigo

--- src/components/shared/AppHeader.jsx
+++ src/components/shared/AppHeader.jsx
@@ -4,15 +4,10 @@ import { Link } from "react-router-dom";
 import useThemeSwitcher from "../../hooks/useThemeSwitcher";
 import logoLight from "../../images/logo-light.png";
 import logoDark from "../../images/logo-dark.png";
-import { motion } from "framer-motion";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import { Button } from "@mui/material";
-
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
 import { useTheme } from "@mui/material/styles";
 
 const AppHeader = () => {
@@ -90,7 +85,6 @@ const AppHeader = () => {
             <Link
               to="/login"
               sx={{
-                // Asumiendo que tienes un color de fuente blanca definido en tu tema
                 fontWeight: "600",
               }}
             >
@@ -113,143 +107,6 @@ const AppHeader = () => {
         </Toolbar>
       </AppBar>
     </Box>
-
-    // <motion.nav
-    //   initial={{ opacity: 0 }}
-    //   animate={{ opacity: 1 }}
-    //   id="nav"
-    //   className="sm:container sm:mx-auto"
-    // >
-    //   <div className="z-10 max-w-screen-lg xl:max-w-screen-xl block sm:flex sm:justify-between sm:items-center py-6">
-    //     {/* Header menu links and small screen hamburger menu */}
-    //     <div className="flex justify-between items-center px-4 sm:px-0">
-    //       <div>
-    //         <Link to="/">
-    //           {activeTheme === "dark" ? (
-    //             <img src={logoDark} style={{ width: "60px" }} alt="Dark Logo" />
-    //           ) : (
-    //             <img
-    //               src={logoLight}
-    //               style={{ width: "60px" }}
-    //               alt="Dark Logo"
-    //             />
-    //           )}
-    //         </Link>
-    //       </div>
-
-    //       {/* Theme switcher small screen */}
-    //       <div
-    //         onClick={() => setTheme(activeTheme)}
-    //         aria-label="Theme Switcher"
-    //         className="block sm:hidden ml-0 bg-primary-light dark:bg-ternary-dark p-3 shadow-sm rounded-xl cursor-pointer"
-    //       >
-    //         {activeTheme === "dark" ? (
-    //           <FiMoon className="text-ternary-dark hover:text-gray-400 dark:text-ternary-light dark:hover:text-primary-light text-xl" />
-    //         ) : (
-    //           <FiSun className="text-gray-200 hover:text-gray-50 text-xl" />
-    //         )}
-    //       </div>
-
-    //       {/* Small screen hamburger menu */}
-    //       <div className="sm:hidden">
-    //         <button
-    //           onClick={toggleMenu}
-    //           type="button"
-    //           className="focus:outline-none"
-    //           aria-label="Hamburger Menu"
-    //         >
-    //           <svg
-    //             xmlns="http://www.w3.org/2000/svg"
-    //             viewBox="0 0 24 24"
-    //             className="h-7 w-7 fill-current text-secondary-dark dark:text-ternary-light"
-    //           >
-    //             {showMenu ? (
-    //               <FiX className="text-3xl" />
-    //             ) : (
-    //               <FiMenu className="text-3xl" />
-    //             )}
-    //           </svg>
-    //         </button>
-    //       </div>
-    //     </div>
-
-    //     {/* Header links small screen */}
-    //     <div
-    //       className={
-    //         showMenu
-    //           ? "block m-0 sm:ml-4 mt-5 sm:mt-3 sm:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none"
-    //           : "hidden"
-    //       }
-    //     >
-    //       <Link
-    //         to="/workers"
-    //         className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
-    //         aria-label="Trabajadores"
-    //       >
-    //         TRABAJADORES
-    //       </Link>
-    //       <Link
-    //         to="/form"
-    //         className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
-    //         aria-label="form"
-    //       >
-    //         ¿TENES UN{" "}
-    //         <span
-    //           style={{
-    //             backgroundImage:
-    //               "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)",
-    //           }}
-    //         >
-    //           TRABAJADOR
-    //         </span>{" "}
-    //         QUE RECOMENDAR?
-    //       </Link>
-    //     </div>
-
-    //     {/* Header links large screen */}
-    //     <div className="font-general-medium hidden m-0 sm:ml-4 mt-5 sm:mt-3 sm:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none">
-    //       <Link
-    //         to="/workers"
-    //         className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
-    //         aria-label="Trabajadores"
-    //       >
-    //         TRABAJADORES
-    //       </Link>
-    //       <Link
-    //         to="/form"
-    //         className="block text-left text-lg text-primary-dark dark:text-ternary-light hover:text-secondary-dark dark:hover:text-secondary-light  sm:mx-4 mb-2 sm:py-2"
-    //         aria-label="form"
-    //       >
-    //         ¿TENES UN{" "}
-    //         <span
-    //           style={{
-    //             backgroundImage:
-    //               "linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%)",
-    //           }}
-    //         >
-    //           TRABAJADOR
-    //         </span>{" "}
-    //         QUE RECOMENDAR?
-    //       </Link>
-    //     </div>
-
-    //     {/* Header right section buttons */}
-    //     <div className="hidden sm:flex justify-between items-center flex-col md:flex-row">
-    //       {/* Theme switcher large screen */}
-    //       <div
-    //         onClick={() => setTheme(activeTheme)}
-    //         aria-label="Theme Switcher"
-    //         className="ml-8 bg-primary-light dark:bg-ternary-dark p-3 shadow-sm rounded-xl cursor-pointer"
-    //       >
-    //         {activeTheme === "dark" ? (
-    //           <FiMoon className="text-ternary-dark hover:text-gray-400 dark:text-ternary-light dark:hover:text-primary-light text-xl" />
-    //         ) : (
-    //           <FiSun className="text-gray-200 hover:text-gray-50 text-xl" />
-    //         )}
-    //       </div>
-    //     </div>
-    //   </div>
-    // </motion.nav>
   );
 };
 
