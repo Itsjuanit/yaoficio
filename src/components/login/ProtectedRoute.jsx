3fd245e22d71f39593d871055b7c82e7d0a1be6b
Juan Ignacio Tejada <tejadajuanignacio12@gmail.com>
2025-03-28 17:27:14 -0300

    Se realizan cambios

--- src/components/login/ProtectedRoute.jsx
+++ src/components/login/ProtectedRoute.jsx
@@ -3,8 +3,6 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = () => {
   const isAuthenticated = Boolean(localStorage.getItem("authToken"));
-
-  // Si no está autenticado, redirige a login
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 };
 
